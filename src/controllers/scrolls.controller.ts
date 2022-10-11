91e621a03b310ff656fa07f8f1cba07ba06f0f5e
Gabe <ggaabe@gmail.com>
2022-10-10 23:11:03 -0500

    Removed console logs

--- src/controllers/scrolls.controller.ts
+++ src/controllers/scrolls.controller.ts
@@ -7,7 +7,7 @@ const redisClient = createClient({ url: `${process.env.REDIS_URL}:6379` });
 
 console.log('Redis URL:', process.env.REDIS_URL);
 
-console.log('Connected to redis 55');
+console.log('Connected to redis.');
 class ScrollsController {
   public addressByAdaHandle = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -21,11 +21,9 @@ class ScrollsController {
         console.log('ERROR: ', e);
       }
       const handle = req.query.handle.toString();
-      console.log('Querying handle:', handle);
-      const cursor = await redisClient.get('_cursor');
-      console.log('Cursor:', cursor);
-      const address = await redisClient.get(req.query.handle.toString());
-      console.log('Address retrieved:', address);
+
+      const address = await redisClient.get(handle);
+
       res.status(200).send({ address });
     } catch (error) {
       console.log('REDIS ERROR:', error);
