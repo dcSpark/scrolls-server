0dafb02489cf494aa5c162bba2fc76565846bc4e
Gabe <ggaabe@gmail.com>
2022-10-10 22:39:23 -0500

    Removes unused code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import App from '@/app';
-import AuthRoute from '@routes/auth.route';
+
 import IndexRoute from '@routes/index.route';
-import UsersRoute from '@routes/users.route';
+
 import validateEnv from '@utils/validateEnv';
 import ScrollsRoute from './routes/scrolls.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new ScrollsRoute()]);
+const app = new App([new IndexRoute(), new ScrollsRoute()]);
 
 app.listen();
